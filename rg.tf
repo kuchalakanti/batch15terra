38d2c57a3a64897b1560d7b90e881b162419e7e0
kuchalakanti <kuchalakanti@gmail.com>
2024-12-12 08:14:15 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -5,3 +5,11 @@ resource "azurerm_resource_group" "example" {
     name = krishna
   }
 }
+
+resource "azurerm_resource_group" "example" {
+  name     = "example1"
+  location = "West Europe1"
+  tags = {
+    name = krishna
+  }
+}

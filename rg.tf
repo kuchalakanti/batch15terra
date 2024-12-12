61756d405f51baaaed2089a39430b5da74441441
Krishna <krishna@gmail.com>
2024-12-12 08:13:30 +0530

    deleted rgnew

--- rg.tf
+++ rg.tf
@@ -5,11 +5,3 @@ resource "azurerm_resource_group" "example" {
     name = krishna
   }
 }
-
-resource "azurerm_resource_group" "example" {
-  name     = "example"
-  location = "West Europe"
-  tags = {
-    name = krishna
-  }
-}

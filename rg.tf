10d24dbb115f6cb0f914ce6e2b2ac2aa323f85ec
Krishna <krishna@gmail.com>
2024-12-12 08:12:04 +0530

    modified

--- rg.tf
+++ rg.tf
@@ -1,4 +1,15 @@
 resource "azurerm_resource_group" "example" {
   name     = "example"
   location = "West Europe"
+  tags = {
+    name = krishna
+  }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example"
+  location = "West Europe"
+  tags = {
+    name = krishna
+  }
 }
